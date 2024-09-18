4243bbec1f05fecb58933ccb1690adf31e11cbe6
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-09-18 20:33:07 -0300

    Estrutura Home Page finalizada

---
+++ src/components/pages/Skills.jsx
@@ -0,0 +1,11 @@
+import styles from "Skills.module.css";
+
+const Skills = () => {
+  return (
+    <div className={styles.container}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Skills;
