a5691995029018c4e4633c4df7252c03e25c3a66
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-10-25 20:23:06 -0300

    Linha de importação removida do arquivo 'i18n.js'

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en.json";
 import ptTranslation from "./locales/pt.json";
-import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
   .use(initReactI18next) // Integração com o React
