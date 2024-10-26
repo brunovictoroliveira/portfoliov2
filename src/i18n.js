9013538c0c5c656b5ec120730476f47c787aad77
Bruno Victor Lima <101837219+brunovictorlima@users.noreply.github.com>
2024-10-26 01:51:41 -0300

    Criação de redirecionamento página 404

--- src/i18n.js
+++ src/i18n.js
@@ -3,22 +3,20 @@ import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en.json";
 import ptTranslation from "./locales/pt.json";
 
-i18n
-  .use(initReactI18next) // Integração com o React
-  .init({
-    lng: "pt-BR", // Definir linguagem inicial
-    fallbacklng: "en", // Definir um idioma de fallback
-    resources: {
-      en: {
-        translation: enTranslation,
-      },
-      pt: {
-        translation: ptTranslation,
-      },
+i18n.use(initReactI18next).init({
+  lng: "pt-BR",
+  fallbacklng: "en",
+  resources: {
+    en: {
+      translation: enTranslation,
     },
-    interpolation: {
-      scapeValue: false, // Evitar a necessidade de escapar essa sequência em traduções
+    pt: {
+      translation: ptTranslation,
     },
-  });
+  },
+  interpolation: {
+    scapeValue: false,
+  },
+});
 
 export default i18n;
